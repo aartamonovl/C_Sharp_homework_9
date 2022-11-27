bdbe4fcde9af1136d3519c931f2782fbd26453ba
a.artamonov <a.artamonov@gb.ru>
2022-11-27 15:40:07 +0300

    Second task is done

--- Program.cs
+++ Program.cs
@@ -2,18 +2,27 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-void ShowNumbers(int n)
-{
-    Console.Write(n + " ");
-    if(n > 1) ShowNumbers(n - 1);
-}
+// void ShowNumbers(int n)
+// {
+//     Console.Write(n + " ");
+//     if(n > 1) ShowNumbers(n - 1);
+// }
 
-ShowNumbers(30);
-Console.WriteLine();
+// ShowNumbers(30);
+// Console.WriteLine();
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int SumNumbers(int m, int n)
+{
+    if (Math.Max(n,m) != Math.Min(n,m))
+        return Math.Min(m,n) + SumNumbers(Math.Min(m, n) + 1, Math.Max(m, n));
+    else return n;
+}
+
+Console.WriteLine(SumNumbers(1, 8));
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
