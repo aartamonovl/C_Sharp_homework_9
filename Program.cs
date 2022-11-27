9f4f5dd3c2d77cd37275b741ef7df5d06b1c92bc
a.artamonov <a.artamonov@gb.ru>
2022-11-27 15:59:57 +0300

    All is commented

--- Program.cs
+++ Program.cs
@@ -28,15 +28,15 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int Akkerman(int m, int n)
-{
-  if (m == 0)
-    return n + 1;
-  else
-    if ((m != 0) && (n == 0))
-      return Akkerman(m - 1, 1);
-    else
-      return Akkerman(m - 1, Akkerman(m, n - 1));
-}
+// int Akkerman(int m, int n)
+// {
+//   if (m == 0)
+//     return n + 1;
+//   else
+//     if ((m != 0) && (n == 0))
+//       return Akkerman(m - 1, 1);
+//     else
+//       return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
 
-Console.WriteLine(Akkerman(3, 1));
+// Console.WriteLine(Akkerman(3, 1));
